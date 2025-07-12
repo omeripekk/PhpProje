fe7f45db4d085fedb33e7835bd7e8199e73e8923
Ömer <omerfarukipek4@icloud.com>
2025-07-12 16:45:23 +0300

    Projeyi güncelledim

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -1,12 +1,13 @@
 <?php
 
 // Veritabanı bağlantı bilgileri
-$vt_sunucu = "localhost";        
+$vt_sunucu = "127.0.0.1";        
 $vt_kullanici = "root";           
 $vt_sifre = "";                  
 $vt_adi = "kullanici_iletisim";  
+$vt_port = 3307;
 
-$baglan = mysqli_connect($vt_sunucu, $vt_kullanici, $vt_sifre, $vt_adi);
+$baglan = mysqli_connect($vt_sunucu, $vt_kullanici, $vt_sifre, $vt_adi, $vt_port);
 
 if (!$baglan) {
     die("Veritabanı bağlantı işlemi başarısız: " . mysqli_connect_error()); // die durdurur
